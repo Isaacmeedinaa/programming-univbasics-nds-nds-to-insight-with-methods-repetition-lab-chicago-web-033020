231294f1d364f08f8c657b0355fe63500eaaa2b5
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-24 03:55:50 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,6 +41,17 @@ end
 
 def total_gross(source)
   
+  total_gross_of_earnings = 0
+  director_index = 0
+
+  while director_index < source.size do
+    
+    total_gross_of_earnings += gross_for_director(source[director_index])
+    
+    director_index += 1
+  end
+  
+  total_gross_of_earnings
 end
 
 
